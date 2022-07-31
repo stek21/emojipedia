80e9f40766a8369644e0a5d7e396cd75e65e7ece
Ekaterina S <jobstek21@gmail.com>
2022-07-31 15:27:18 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Emoji from "./Emoji";
+import emojipedia from "../emojipedia";
+
+function createEmoji(emojiTerm) {
+  return (
+  <Emoji
+  key={emojiTerm.id}
+  emoji={emojiTerm.emoji}
+  name={emojiTerm.name}
+  description ={emojiTerm.meaning}
+  />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createEmoji)}        
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
+  },
+  {
+    id: 5,
+    emoji: "😃",
+    name: "Smiling Face With Open Mouth",
+    meaning:
+      "Typical smiley face with open mouth and oval eyes. Is in a positive mood, shows its teeth and laughs cheerfully. Expresses enthusiasm: from a cheerful greeting to boundless joy."
+  },
+  {
+    id: 6,
+    emoji: "😄",
+    name: "Smiling Face With Open Mouth And Smiling Eyes",
+    meaning:
+      "Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm."
+  },
+  {
+    id: 7,
+    emoji: "😁",
+    name: "Grinning Face With Smiling Eyes",
+    meaning:
+      "Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed."
+  },
+  {
+    id: 8,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person. Means the same as “XD“ and “X“ as a text-based emoticon."
+  },
+  {
+    id: 9,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+  },
+  {
+    id: 10,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. "
+  },
+  {
+    id: 11,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning:
+      "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude."
+  },
+  {
+    id: 12,
+    emoji: "😚",
+    name: "Kissing Face With Closed Eyes",
+    meaning:
+      "Cute face with closed eyes and rosy cheeks. As a thank you for a tip or a favor. Loving kiss to the closest friends, family or darling."
+  }
+];
+
+export default emojipedia
